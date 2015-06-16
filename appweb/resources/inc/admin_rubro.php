22fdd63b0eec314f3ed7eec274b7629a80351856
zesar89 <zesar.rastelli@gmail.com>
2015-06-16 01:23:06 -0300

    comenzando con abm de rubros

---
+++ appweb/resources/inc/admin_rubro.php
@@ -0,0 +1,90 @@
+<?php
+require "../../config/ini.php";
+$ArrInfoPage = [ "page-title" => "Rubros", "menu-class" => "page-rubro" ];
+
+try {
+    Fn::FnCheckAccess(1);
+    $ObjRubro = new Rubro();
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+?>
+
+<?php require ROOT_DIR._DIR_TMP_."admin_header.php"; ?>
+<?php require ROOT_DIR._DIR_TMP_."admin_menu.php"; ?>
+
+<div class="container">
+    <!-- left, vertical navbar & content -->
+    <div class="row">
+        <!-- content -->
+        <div class="col-xs-12">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="page-header">
+                        <h1><?php echo $ArrInfoPage["page-title"]; ?></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12 content">
+                    <div class="alert alert-info">
+                        <a class="close" data-dismiss="alert" href="#">×</a>
+                        <span>
+                            Listado de todas los rubros cargados en la aplicación.
+                        </span>
+                    </div>
+                </div>
+            </div> 
+            
+            <!-- Listado principal -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="text-muted bootstrap-admin-box-title">Listado de <?php echo $ArrInfoPage["page-title"]; ?></div>
+                        </div>
+                        <div class="bootstrap-admin-panel-content">
+                            <table class="table table-striped table-bordered" id="listado-rubros">
+                                <thead>
+                                    <tr>
+                                        <th>Ícono</th>
+                                        <th>Descripción</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($ObjRubro->getRubros() as $row): ?>
+                                        <tr>
+                                            <td><?php echo $row->icono; ?></td>
+                                            <td><?php echo $row->descripcion; ?></td>
+                                            <td>
+                                                <button class="btn btn-sm btn-primary" alt="Modificar" title="Modificar">
+                                                    <i class="glyphicon glyphicon-pencil"></i>
+                                                </button>
+                                                
+                                                <button class="btn btn-sm btn-warning" alt="Consultar" title="Consultar">
+                                                    <i class="glyphicon glyphicon-bell"></i>
+                                                </button>
+                                                
+                                                <button class="btn btn-sm btn-danger" alt="Eliminar" title="Eliminar">
+                                                    <i class="glyphicon glyphicon-trash"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>                             
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Listado principal -->
+            
+        </div>
+    </div>
+</div>
+
+
+<?php require ROOT_DIR._DIR_TMP_."admin_footer.php"; ?>
