8a5c35836bd86c6fb5df9ca8e7972ef0dee8649d
zesar89 <zesar.rastelli@gmail.com>
2015-07-05 18:10:11 -0300

    export SQL

--- __sql/script.sql
+++ __sql/script.sql
@@ -1,19 +1,9 @@
-SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE TABLE Calificacion_rel_empresa;
-TRUNCATE TABLE Imagen_rel_empresa;
-TRUNCATE TABLE Video_rel_empresa;
-TRUNCATE TABLE Rubro;
-TRUNCATE TABLE Zona;
-TRUNCATE TABLE Empresa;
-TRUNCATE TABLE Usuario;
-SET FOREIGN_KEY_CHECKS = 1;
-
 -- phpMyAdmin SQL Dump
 -- version 3.5.8.1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 16-06-2015 a las 00:53:51
+-- Tiempo de generación: 05-07-2015 a las 18:06:22
 -- Versión del servidor: 5.5.32
 -- Versión de PHP: 5.4.17
 
@@ -33,6 +23,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `Calificacion_rel_empresa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idempresa` int(11) NOT NULL,
+  `idusuariofb` int(11) NOT NULL,
   `calificacion` tinyint(4) NOT NULL DEFAULT '1',
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -49,17 +40,17 @@ CREATE TABLE IF NOT EXISTS `Calificacion_rel_empresa` (
 CREATE TABLE IF NOT EXISTS `Empresa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_referente` varchar(250) DEFAULT NULL,
-  `dni_referente` varchar(55) DEFAULT NULL,
+  `dni_referente` varchar(50) DEFAULT NULL,
   `nombre` varchar(250) DEFAULT NULL,
   `razon_social` varchar(250) DEFAULT NULL,
   `logo` varchar(250) DEFAULT NULL,
   `email` varchar(150) NOT NULL,
-  `pw` varchar(55) NOT NULL,
-  `telefono` varchar(55) DEFAULT NULL,
+  `pw` varchar(50) NOT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
   `idzona` int(11) DEFAULT NULL,
   `idrubro` int(11) DEFAULT NULL,
   `direccion` varchar(250) DEFAULT NULL,
-  `lat_long` varchar(150) DEFAULT NULL,
+  `lat_long` varchar(250) DEFAULT NULL,
   `descripcion` text,
   `habilitado` tinyint(4) NOT NULL DEFAULT '0',
   `es_premium` tinyint(4) NOT NULL DEFAULT '0',
@@ -69,14 +60,7 @@ CREATE TABLE IF NOT EXISTS `Empresa` (
   KEY `idzona` (`idzona`),
   KEY `idzona_2` (`idzona`),
   KEY `idrubro` (`idrubro`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Volcado de datos para la tabla `Empresa`
---
-
-INSERT INTO `Empresa` (`id`, `nombre_referente`, `dni_referente`, `nombre`, `razon_social`, `logo`, `email`, `pw`, `telefono`, `idzona`, `idrubro`, `direccion`, `lat_long`, `descripcion`, `habilitado`, `es_premium`, `estado`, `fecha`) VALUES
-(1, 'César Rastelli', '34111111', 'Mi comercio', 'Mi comercio', 'logo364_adidas-wallpapasWallpapers4Desktop.com.jpg', 'empresa@empresa.com', 'empresa', '55555555', 1, 1, 'Av. Cordoba 4252, Palermo', '-34.58785408317702, -58.43964992370013', '', 0, 0, 1, '2015-04-25 04:28:00');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -85,15 +69,17 @@ INSERT INTO `Empresa` (`id`, `nombre_referente`, `dni_referente`, `nombre`, `raz
 --
 
 CREATE TABLE IF NOT EXISTS `Imagen_rel_empresa` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idempresa` int(11) NOT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `imagen` varchar(255) DEFAULT NULL,
+  `titulo` varchar(250) DEFAULT NULL,
+  `descripcion` varchar(250) DEFAULT NULL,
+  `link_imagen` varchar(250) DEFAULT NULL,
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   `habilitado` tinyint(4) NOT NULL DEFAULT '0',
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   KEY `idempresa` (`idempresa`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -103,19 +89,12 @@ CREATE TABLE IF NOT EXISTS `Imagen_rel_empresa` (
 
 CREATE TABLE IF NOT EXISTS `Rubro` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(150) DEFAULT NULL,
-  `icono` varchar(150) DEFAULT NULL,
+  `descripcion` varchar(250) DEFAULT NULL,
+  `icono` varchar(250) DEFAULT NULL,
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   `habilitado` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Volcado de datos para la tabla `Rubro`
---
-
-INSERT INTO `Rubro` (`id`, `descripcion`, `icono`, `estado`, `habilitado`) VALUES
-(1, 'Otros', 'otros.png', 1, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -126,21 +105,16 @@ INSERT INTO `Rubro` (`id`, `descripcion`, `icono`, `estado`, `habilitado`) VALUE
 CREATE TABLE IF NOT EXISTS `Usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(250) DEFAULT NULL,
-  `email` varchar(150) NOT NULL,
-  `pw` varchar(55) NOT NULL,
-  `telefono` varchar(55) DEFAULT NULL,
+  `email` varchar(250) NOT NULL,
+  `pw` varchar(50) NOT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
   `direccion` varchar(250) DEFAULT NULL,
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Volcado de datos para la tabla `Usuario`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 INSERT INTO `Usuario` (`id`, `nombre`, `email`, `pw`, `telefono`, `direccion`, `estado`, `fecha`) VALUES
-(1, 'César Rastelli', 'admin@admin.com', 'admin', '1511111111', 'Av. Congreso 1050', 1, '2015-04-25 03:39:29');
+(1, 'César Rastelli', 'admin@admin.com', '91f5167c34c400758115c2a6826ec2e3', '1511111111', 'Av. Congreso 1050', 1, '2015-04-25 03:39:29');
 
 -- --------------------------------------------------------
 
@@ -151,14 +125,15 @@ INSERT INTO `Usuario` (`id`, `nombre`, `email`, `pw`, `telefono`, `direccion`, `
 CREATE TABLE IF NOT EXISTS `Video_rel_empresa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idempresa` int(11) NOT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `codigo` varchar(150) DEFAULT NULL,
+  `titulo` varchar(250) DEFAULT NULL,
+  `descripcion` varchar(250) DEFAULT NULL,
+  `codigo` varchar(20) DEFAULT NULL,
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   `habilitado` tinyint(4) NOT NULL DEFAULT '0',
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `idempresa` (`idempresa`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -168,19 +143,12 @@ CREATE TABLE IF NOT EXISTS `Video_rel_empresa` (
 
 CREATE TABLE IF NOT EXISTS `Zona` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(150) DEFAULT NULL,
-  `coordenadas` text,
+  `descripcion` varchar(250) DEFAULT NULL,
+  `lat_long` varchar(250) DEFAULT NULL,
   `estado` tinyint(4) NOT NULL DEFAULT '1',
   `habilitado` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Volcado de datos para la tabla `Zona`
---
-
-INSERT INTO `Zona` (`id`, `descripcion`, `coordenadas`, `estado`, `habilitado`) VALUES
-(1, 'Capital Federal / GBA', '-34.523252132,-54.523252132', 1, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Restricciones para tablas volcadas
